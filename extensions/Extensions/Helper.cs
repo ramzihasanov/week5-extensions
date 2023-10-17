1a387a250822c7ac0f9029a1b6e452d0c74c8e21
ramzihasanov <bd54tkjwr@code.edu.az>
2023-10-18 01:34:50 +0400

    extensions

---
+++ extensions/Extensions/Helper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensions.Extensions
+{
+    public static class Helper
+    {
+        public static int Repeated(this string sentence, string word)
+        {
+            int count = 0;
+            sentence = sentence.Trim();
+            string[] str = sentence.Split(' ',',');
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == word)
+                    count++;
+            }
+            return count;
+        }
+
+
+
+
+        public static bool IsPolendrom(this int num)
+        {
+
+            int a = 0;
+            int oldnum=num;
+          while(num > 0)
+            {
+               int r=num%10;
+                a = a * 10 + r;
+                num /=10;
+            }
+            return (a == oldnum);
+        }
+
+
+
+
+
+        public static bool HasAnyDigit(this string str)
+        {
+            foreach (char s in str)
+            {
+                if (char.IsDigit(s))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }     
+    }
+}
---
+++ extensions/Program.cs
@@ -0,0 +1,30 @@
+﻿using extensions.Extensions;
+
+namespace extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                                           BEST OPTIMAL CODE");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("========task1=========");
+            string task = "Salam BB206, necəsiniz BB206 əhli ";
+            Console.WriteLine(task.Repeated("BB206"));
+            Console.WriteLine("\n\n\n");
+
+
+            Console.WriteLine("========task2=========");
+            int num = 1221;
+            Console.WriteLine( num.IsPolendrom() );
+            Console.WriteLine("\n\n\n");
+
+
+            Console.WriteLine("========task3=========");
+            string word = "eccghcf 4fcfgc";
+            Console.WriteLine(word.HasAnyDigit() );
+            Console.WriteLine("\n\n");
+            
+        }
+    }
+}
